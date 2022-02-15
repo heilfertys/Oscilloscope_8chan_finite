ff4ac9af8638c8d701ba51a6fa3b114d476880bf
heilfertys <heilfertys@PHY-GWH244-02>
2022-02-15 14:55:02 -0500

    Add project files.

---
+++ Oscilloscope_8chan_finite/Form1.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace Oscilloscope_8chan_finite
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.btnAcquire = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.cboDevice = new System.Windows.Forms.ComboBox();
+            this.cboTermConfig = new System.Windows.Forms.ComboBox();
+            this.cboVoltageRange = new System.Windows.Forms.ComboBox();
+            this.nudLowChan = new System.Windows.Forms.NumericUpDown();
+            this.nudSampRate = new System.Windows.Forms.NumericUpDown();
+            this.nudSampsPerChan = new System.Windows.Forms.NumericUpDown();
+            this.nudHighChan = new System.Windows.Forms.NumericUpDown();
+            this.chtChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.lblDevices = new System.Windows.Forms.Label();
+            this.lblVoltageRange = new System.Windows.Forms.Label();
+            this.lblTermConfig = new System.Windows.Forms.Label();
+            this.lblSampleRate = new System.Windows.Forms.Label();
+            this.lblLowChan = new System.Windows.Forms.Label();
+            this.lblSampsPerChan = new System.Windows.Forms.Label();
+            this.lblHighChan = new System.Windows.Forms.Label();
+            this.lblAqTime = new System.Windows.Forms.Label();
+            this.lblADRate = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.nudLowChan)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudSampRate)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudSampsPerChan)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudHighChan)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chtChart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAcquire
+            // 
+            this.btnAcquire.Location = new System.Drawing.Point(78, 302);
+            this.btnAcquire.Name = "btnAcquire";
+            this.btnAcquire.Size = new System.Drawing.Size(123, 78);
+            this.btnAcquire.TabIndex = 0;
+            this.btnAcquire.Text = "Acquire";
+            this.btnAcquire.UseVisualStyleBackColor = true;
+            this.btnAcquire.Click += new System.EventHandler(this.btnAcquire_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(256, 302);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(123, 78);
+            this.btnClear.TabIndex = 1;
+            this.btnClear.Text = "Clear Chart";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // cboDevice
+            // 
+            this.cboDevice.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboDevice.FormattingEnabled = true;
+            this.cboDevice.Location = new System.Drawing.Point(77, 52);
+            this.cboDevice.Name = "cboDevice";
+            this.cboDevice.Size = new System.Drawing.Size(145, 21);
+            this.cboDevice.TabIndex = 2;
+            // 
+            // cboTermConfig
+            // 
+            this.cboTermConfig.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboTermConfig.FormattingEnabled = true;
+            this.cboTermConfig.Location = new System.Drawing.Point(77, 99);
+            this.cboTermConfig.Name = "cboTermConfig";
+            this.cboTermConfig.Size = new System.Drawing.Size(145, 21);
+            this.cboTermConfig.TabIndex = 3;
+            // 
+            // cboVoltageRange
+            // 
+            this.cboVoltageRange.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboVoltageRange.FormattingEnabled = true;
+            this.cboVoltageRange.Location = new System.Drawing.Point(300, 52);
+            this.cboVoltageRange.Name = "cboVoltageRange";
+            this.cboVoltageRange.Size = new System.Drawing.Size(154, 21);
+            this.cboVoltageRange.TabIndex = 4;
+            // 
+            // nudLowChan
+            // 
+            this.nudLowChan.Location = new System.Drawing.Point(76, 150);
+            this.nudLowChan.Name = "nudLowChan";
+            this.nudLowChan.Size = new System.Drawing.Size(145, 20);
+            this.nudLowChan.TabIndex = 5;
+            this.nudLowChan.ValueChanged += new System.EventHandler(this.nudLowChan_ValueChanged);
+            // 
+            // nudSampRate
+            // 
+            this.nudSampRate.Location = new System.Drawing.Point(300, 100);
+            this.nudSampRate.Name = "nudSampRate";
+            this.nudSampRate.Size = new System.Drawing.Size(154, 20);
+            this.nudSampRate.TabIndex = 6;
+            // 
+            // nudSampsPerChan
+            // 
+            this.nudSampsPerChan.Location = new System.Drawing.Point(300, 150);
+            this.nudSampsPerChan.Name = "nudSampsPerChan";
+            this.nudSampsPerChan.Size = new System.Drawing.Size(154, 20);
+            this.nudSampsPerChan.TabIndex = 7;
+            // 
+            // nudHighChan
+            // 
+            this.nudHighChan.Location = new System.Drawing.Point(76, 197);
+            this.nudHighChan.Name = "nudHighChan";
+            this.nudHighChan.Size = new System.Drawing.Size(144, 20);
+            this.nudHighChan.TabIndex = 8;
+            this.nudHighChan.ValueChanged += new System.EventHandler(this.nudHighChan_ValueChanged);
+            // 
+            // chtChart
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chtChart.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chtChart.Legends.Add(legend1);
+            this.chtChart.Location = new System.Drawing.Point(484, 64);
+            this.chtChart.Name = "chtChart";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chtChart.Series.Add(series1);
+            this.chtChart.Size = new System.Drawing.Size(541, 370);
+            this.chtChart.TabIndex = 9;
+            this.chtChart.Text = "chart1";
+            // 
+            // lblDevices
+            // 
+            this.lblDevices.AutoSize = true;
+            this.lblDevices.Location = new System.Drawing.Point(74, 37);
+            this.lblDevices.Name = "lblDevices";
+            this.lblDevices.Size = new System.Drawing.Size(49, 13);
+            this.lblDevices.TabIndex = 10;
+            this.lblDevices.Text = "Devices:";
+            // 
+            // lblVoltageRange
+            // 
+            this.lblVoltageRange.AutoSize = true;
+            this.lblVoltageRange.Location = new System.Drawing.Point(298, 37);
+            this.lblVoltageRange.Name = "lblVoltageRange";
+            this.lblVoltageRange.Size = new System.Drawing.Size(81, 13);
+            this.lblVoltageRange.TabIndex = 11;
+            this.lblVoltageRange.Text = "Voltage Range:";
+            // 
+            // lblTermConfig
+            // 
+            this.lblTermConfig.AutoSize = true;
+            this.lblTermConfig.Location = new System.Drawing.Point(75, 84);
+            this.lblTermConfig.Name = "lblTermConfig";
+            this.lblTermConfig.Size = new System.Drawing.Size(115, 13);
+            this.lblTermConfig.TabIndex = 12;
+            this.lblTermConfig.Text = "Terminal Configuration:";
+            // 
+            // lblSampleRate
+            // 
+            this.lblSampleRate.AutoSize = true;
+            this.lblSampleRate.Location = new System.Drawing.Point(298, 85);
+            this.lblSampleRate.Name = "lblSampleRate";
+            this.lblSampleRate.Size = new System.Drawing.Size(113, 13);
+            this.lblSampleRate.TabIndex = 13;
+            this.lblSampleRate.Text = "Channel Sample Rate:";
+            // 
+            // lblLowChan
+            // 
+            this.lblLowChan.AutoSize = true;
+            this.lblLowChan.Location = new System.Drawing.Point(75, 135);
+            this.lblLowChan.Name = "lblLowChan";
+            this.lblLowChan.Size = new System.Drawing.Size(72, 13);
+            this.lblLowChan.TabIndex = 14;
+            this.lblLowChan.Text = "Low Channel:";
+            // 
+            // lblSampsPerChan
+            // 
+            this.lblSampsPerChan.AutoSize = true;
+            this.lblSampsPerChan.Location = new System.Drawing.Point(298, 135);
+            this.lblSampsPerChan.Name = "lblSampsPerChan";
+            this.lblSampsPerChan.Size = new System.Drawing.Size(140, 13);
+            this.lblSampsPerChan.TabIndex = 15;
+            this.lblSampsPerChan.Text = "Number Samples / Channel:";
+            // 
+            // lblHighChan
+            // 
+            this.lblHighChan.AutoSize = true;
+            this.lblHighChan.Location = new System.Drawing.Point(75, 182);
+            this.lblHighChan.Name = "lblHighChan";
+            this.lblHighChan.Size = new System.Drawing.Size(74, 13);
+            this.lblHighChan.TabIndex = 16;
+            this.lblHighChan.Text = "High Channel:";
+            this.lblHighChan.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // lblAqTime
+            // 
+            this.lblAqTime.AutoSize = true;
+            this.lblAqTime.Location = new System.Drawing.Point(283, 193);
+            this.lblAqTime.Name = "lblAqTime";
+            this.lblAqTime.Size = new System.Drawing.Size(101, 13);
+            this.lblAqTime.TabIndex = 17;
+            this.lblAqTime.Text = "Acquisition Time (s):";
+            // 
+            // lblADRate
+            // 
+            this.lblADRate.AutoSize = true;
+            this.lblADRate.Location = new System.Drawing.Point(283, 233);
+            this.lblADRate.Name = "lblADRate";
+            this.lblADRate.Size = new System.Drawing.Size(56, 13);
+            this.lblADRate.TabIndex = 18;
+            this.lblADRate.Text = "A/D Rate:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1054, 527);
+            this.Controls.Add(this.lblADRate);
+            this.Controls.Add(this.lblAqTime);
+            this.Controls.Add(this.lblHighChan);
+            this.Controls.Add(this.lblSampsPerChan);
+            this.Controls.Add(this.lblLowChan);
+            this.Controls.Add(this.lblSampleRate);
+            this.Controls.Add(this.lblTermConfig);
+            this.Controls.Add(this.lblVoltageRange);
+            this.Controls.Add(this.lblDevices);
+            this.Controls.Add(this.chtChart);
+            this.Controls.Add(this.nudHighChan);
+            this.Controls.Add(this.nudSampsPerChan);
+            this.Controls.Add(this.nudSampRate);
+            this.Controls.Add(this.nudLowChan);
+            this.Controls.Add(this.cboVoltageRange);
+            this.Controls.Add(this.cboTermConfig);
+            this.Controls.Add(this.cboDevice);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnAcquire);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nudLowChan)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudSampRate)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudSampsPerChan)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudHighChan)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chtChart)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAcquire;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.ComboBox cboDevice;
+        private System.Windows.Forms.ComboBox cboTermConfig;
+        private System.Windows.Forms.ComboBox cboVoltageRange;
+        private System.Windows.Forms.NumericUpDown nudLowChan;
+        private System.Windows.Forms.NumericUpDown nudSampRate;
+        private System.Windows.Forms.NumericUpDown nudSampsPerChan;
+        private System.Windows.Forms.NumericUpDown nudHighChan;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chtChart;
+        private System.Windows.Forms.Label lblDevices;
+        private System.Windows.Forms.Label lblVoltageRange;
+        private System.Windows.Forms.Label lblTermConfig;
+        private System.Windows.Forms.Label lblSampleRate;
+        private System.Windows.Forms.Label lblLowChan;
+        private System.Windows.Forms.Label lblSampsPerChan;
+        private System.Windows.Forms.Label lblHighChan;
+        private System.Windows.Forms.Label lblAqTime;
+        private System.Windows.Forms.Label lblADRate;
+    }
+}
+
---
+++ Oscilloscope_8chan_finite/Form1.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NationalInstruments.DAQmx;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Oscilloscope_8chan_finite
+{
+    public partial class Form1 : Form
+    {
+        AnalogMultiChannelReader reader;
+        NationalInstruments.DAQmx.Task readTask;
+
+        //NationalInstruments.DAQmx.A2D_MAXTIME = 10;
+        //A2D_MAXRATE = 10;
+
+        double rangeMinimum;
+        double rangeMaximum;
+        string strTermConfig;
+        int termConfigNum;
+        double[,] dataIn;
+        
+        int numberOfSamples;
+        int sampleRate;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cboDevice.Items.AddRange(DaqSystem.Local.Devices);
+            cboDevice.DropDownStyle = ComboBoxStyle.DropDownList;
+
+            cboTermConfig.Items.Add("Differential");
+            cboTermConfig.Items.Add("NRSE");
+            cboTermConfig.Items.Add("RSE");
+            cboTermConfig.SelectedIndex = 0;
+
+
+            cboVoltageRange.Items.Add("+/- 0.2");
+            cboVoltageRange.Items.Add("+/- 1");
+            cboVoltageRange.Items.Add("+/- 5");
+            cboVoltageRange.Items.Add("+/- 10");
+            cboVoltageRange.SelectedIndex = 3;
+
+            cboDevice.SelectedIndex = 0;
+
+
+
+            nudHighChan.Maximum = 7;
+            nudHighChan.Minimum = 0;
+            nudLowChan.Maximum = 7;
+            nudLowChan.Minimum = 0;
+            nudHighChan.Value = 0;
+            nudLowChan.Value = 0;
+
+            nudSampRate.Maximum = 500000;
+            nudSampRate.Minimum = 1;
+            nudSampRate.Value = 10000;
+            nudSampsPerChan.Value = 100;
+            nudSampsPerChan.Maximum = 10000;
+            nudSampsPerChan.Minimum = 1;
+
+            Title title = chtChart.Titles.Add("Voltage vs Time");
+            title.Font = new System.Drawing.Font("Arial", 14, FontStyle.Bold);
+            title.ForeColor = System.Drawing.Color.FromArgb(255, 0, 255); // rgb color code (red, green, blue)
+
+            // start X axes at zero
+            chtChart.ChartAreas[0].AxisX.Minimum = 0.0;
+            
+
+
+            // change font of NUMBERS on x axis
+            chtChart.ChartAreas[0].AxisX.LabelStyle.Font = new System.Drawing.Font("Arial", 14, FontStyle.Bold);
+            // change font of numbers on y axis
+            chtChart.ChartAreas[0].AxisY.LabelStyle.Font = new System.Drawing.Font("Arial", 14, FontStyle.Bold);
+
+            chtChart.ChartAreas[0].AxisX.Title = "Time(s)";
+            chtChart.ChartAreas[0].AxisX.TitleFont = new System.Drawing.Font("Arial", 14, FontStyle.Bold);
+            chtChart.ChartAreas[0].AxisY.Title = "Voltage(volts)";
+            chtChart.ChartAreas[0].AxisY.TitleFont = new System.Drawing.Font("Arial", 14, FontStyle.Bold);
+            // Set up legend
+            chtChart.Legends[0].Alignment = StringAlignment.Center;
+            chtChart.Legends[0].Docking = Docking.Right;
+            chtChart.Legends[0].LegendStyle = LegendStyle.Table;
+            chtChart.Legends[0].TableStyle = LegendTableStyle.Auto;
+            chtChart.Legends[0].Font = new System.Drawing.Font("Arial", 12, FontStyle.Bold);
+
+
+        }
+
+        private void btnAcquire_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                btnAcquire.Enabled = false;
+                switch (cboVoltageRange.SelectedIndex)
+                {
+                    case 0:
+                        rangeMinimum = -0.2;
+                        rangeMaximum = 0.2;
+                        break;
+
+                    case 1:
+                        rangeMinimum = -1;
+                        rangeMaximum = 1;
+                        break;
+
+                    case 2:
+                        rangeMinimum = -5;
+                        rangeMaximum = 5;
+                        break;
+
+                    case 3:
+                        rangeMinimum = -10;
+                        rangeMaximum = 10;
+                        break;
+
+                    default:
+                        rangeMinimum = -10;
+                        rangeMaximum = 10;
+                        break;
+                }
+
+                //strTermConfig
+
+                //switch (cboTermConfig.SelectedIndex)
+                //{
+                //    case 0:
+                //        strTermConfig = AITerminalConfiguration.Differential;
+                //        break;
+                //    case 1:
+                //        termConfigNum = 10078;
+                //        break;
+                //    case 2:
+                //        termConfigNum = 10083;
+                //        break;
+                //}
+
+
+                //switch (cboTermConfig.SelectedIndex)
+                //{
+                //    case 0:
+                //        termConfigNum = 10106;
+                //        break;
+                //    case 1:
+                //        termConfigNum = 10078;
+                //        break;
+                //    case 2:
+                //        termConfigNum = 10083;
+                //        break;
+                //}
+
+                sampleRate = (int)nudSampRate.Value;
+                numberOfSamples = (int)nudSampsPerChan.Value;
+                dataIn = new double[(int)(nudHighChan.Value - (int)nudLowChan.Value + 1), numberOfSamples];
+                readTask = new NationalInstruments.DAQmx.Task();
+
+
+                //MessageBox.Show(cboDevice.SelectedItem.ToString() + "/ai" + nudLowChan.Value.ToString() + ":" + nudHighChan.Value.ToString());
+                readTask.AIChannels.CreateVoltageChannel(cboDevice.SelectedItem.ToString() + "/ai" + nudLowChan.Value.ToString() + ":" + nudHighChan.Value.ToString(), "",
+                   AITerminalConfiguration.Differential, rangeMinimum, rangeMaximum, AIVoltageUnits.Volts);
+
+
+                readTask.Timing.ConfigureSampleClock(
+                    "", // external clock source line or use "" for internal clock
+                    (double)nudSampRate.Value, // expected rate of external clock or actual rate of internal clock
+                    SampleClockActiveEdge.Rising, // acquire on rising or falling edge of ticks
+                    SampleQuantityMode.FiniteSamples, // continuous or finite samples
+                    (int)nudSampsPerChan.Value // number of finite samples to acquire or used for buffer size if continuous
+                    );
+                
+                //Create the reader
+                reader = new AnalogMultiChannelReader(readTask.Stream);
+                if (((nudHighChan.Value - nudLowChan.Value + 1) * sampleRate) > 500000)
+                {
+                    MessageBox.Show("(Number of Channels x Sample Rate) Must Be Less Than 500,000.");
+                    btnAcquire.Enabled = true;
+                }
+                else {
+                    IAsyncResult handle = reader.BeginReadMultiSample((int)nudSampsPerChan.Value, new AsyncCallback(OnDataReady), null);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                btnAcquire.Enabled = true;
+            }
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void nudLowChan_ValueChanged(object sender, EventArgs e)
+        {
+            if (nudHighChan.Value < nudLowChan.Value)
+            {
+                MessageBox.Show("High Channel must be greater thatn Low Channel.");
+                nudLowChan.Value = nudLowChan.Value - 1;
+            }
+        }
+        void OnDataReady(IAsyncResult i)
+        {
+            try
+            {
+                //Retrieve the data that was read.
+                //At this point, any exceptions that occurred during the asynchronous read are thrown
+                dataIn = reader.EndReadMultiSample(i);
+                
+
+                //if (nudHighChan.Value - nudLowChan.Value > 0) DAQmxAsyncRead(readTask);
+
+                double[] time = new double[numberOfSamples];
+                chtChart.ChartAreas[0].AxisY.Minimum = rangeMinimum-1;
+                chtChart.ChartAreas[0].AxisY.Maximum = rangeMaximum+1;
+
+                while (chtChart.Series.Count > 0) chtChart.Series.RemoveAt(0);
+                for (int k = 0; k < numberOfSamples; k++)
+                    time[k] = (double)k / (double)sampleRate;
+                for (int k = 0; k < (nudHighChan.Value - nudLowChan.Value + 1); k++)
+                {
+                    chtChart.Series.Add("Channel" + k.ToString());
+                    chtChart.Series["Channel" + k.ToString()].ChartType = SeriesChartType.Line;
+                }
+
+                // Add data to each series, 
+                for (int k = 0; k < (nudHighChan.Value - nudLowChan.Value + 1); k++)
+                {
+                    for (int j = 0; j < numberOfSamples; j++)
+                    {
+                        chtChart.Series["Channel" + k.ToString()].Points.AddXY(time[j], dataIn[k, j]);
+                    }
+                }
+                double sampleTime = (double)numberOfSamples/(double)sampleRate;
+                lblAqTime.Text = "Acquisition Time (s): "+sampleTime.ToString(); 
+                lblADRate.Text = "A/D Rate: "+ ((nudHighChan.Value - nudLowChan.Value + 1)*sampleRate).ToString();
+
+                btnAcquire.Enabled = true;
+
+                if (readTask != null)  // only dispose if task exists
+                {
+                    readTask.Dispose();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void nudHighChan_ValueChanged(object sender, EventArgs e)
+        {
+            if (nudHighChan.Value < nudLowChan.Value)
+            {
+                MessageBox.Show("High Channel must be greater thatn Low Channel.");
+                nudHighChan.Value = nudHighChan.Value + 1;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                if (readTask != null)  // only dispose if task exists
+                {
+                    readTask.Dispose();
+                }
+            }
+            catch(Exception ex) 
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
